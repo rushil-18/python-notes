b9cb07ca47293e4dd4c4ff40a1b7ee322226ccf0
rushil-18 <mrushil18@gmail.com>
2026-07-06 20:31:47 +0530

    Initial commit

---
+++ modules.py
@@ -0,0 +1,12 @@
+#modules 
+#A module is a file containing a set of codes or a set of
+#functions which can be included to an application. A module could be a file containing
+#a single variable, a function or a big code base.
+
+
+# from math import sqrt
+# file to import function 
+
+
+#import os, math, statistics *, random, sys, time, datetime, json, re, requests, 
+# urllib, socket, subprocess, shutil
